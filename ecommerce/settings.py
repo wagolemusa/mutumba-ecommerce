d9956ef5a4604e1df3fb2db479200fbed9b2095e
kisumutalent <homiemusa@gmail.com>
2020-05-28 14:29:36 +0300

    All Design

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,9 +129,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
     # '/var/www/static/',
 ]
+
+# STATICFILES_DIRS = [
+#     "/mutumba-ecommerce/ecommerce/static",
+# ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 MEDIA_URL = "/media/"
