af35116b471ea7217d3fa8562c4b854bec6c4ff8
kisumutalent <homiemusa@gmail.com>
2020-12-25 17:59:10 +0300

    Make it work

--- shops/views.py
+++ shops/views.py
@@ -364,7 +364,7 @@ class Mpesa(LoginRequiredMixin, View):
 
 # @method_decorator(login_required, name='dispatch')
 @csrf_exempt
-def callbackurl(LoginRequiredMixin, request):
+def callbackurl(request):
 	"""
 	It recieves the response from safaricam
 	"""
