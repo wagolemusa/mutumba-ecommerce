9f36185b1bc7c45d811e7fa340035f3df8f7c6ca
kisumutalent <homiemusa@gmail.com>
2020-12-28 00:14:10 +0300

    Make it work

--- shops/views.py
+++ shops/views.py
@@ -392,7 +392,7 @@ def callbackurl(request):
 	status = pay()
 	print(status)
 
-	callback = Mpesapay.objects.filter(user = self.request.user, cash='notpayed')
+	callback = Mpesapay.objects.filter(user, cash='notpayed')
 	callback.update(cash=status)
 		
 	if status == 'Paid':
@@ -401,7 +401,7 @@ def callbackurl(request):
 		# def get(self, *args, **kwargs):
 		# order = Order.objects.filter(user = request.user, ordered='False')
 		# print(order)
-		order = Order.objects.get(user = self.request.user, ordered=False)
+		order = Order.objects.get(user, ordered=False)
 		order.update(ordered=True)
 		for item in order:
 			item.save()
