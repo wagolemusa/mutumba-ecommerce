cfda90399326b72bbcd516a789e8139efc5a51f1
kisumutalent <homiemusa@gmail.com>
2020-12-27 17:10:22 +0300

    Make it work

--- shops/views.py
+++ shops/views.py
@@ -367,7 +367,7 @@ class Mpesa(LoginRequiredMixin, View):
 def callbackurl(request):
 	# def get(self, *args, **kwargs):
 	# 	# def callbackurl(self, request, *args, **kwargs):
-	# 	current_user = self.request.user
+	current_user = request.user
 	# 	print(current_user.username)
 	# return HttpResponse("Welcome to poll's index!")
 	"""
