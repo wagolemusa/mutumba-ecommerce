54440cb6408a7e352d582e307ae05f622d57e3e3
kisumutalent <homiemusa@gmail.com>
2020-12-29 07:42:13 +0300

    Get user

--- shops/views.py
+++ shops/views.py
@@ -362,14 +362,14 @@ class Mpesa(LoginRequiredMixin, View):
 			messages.error(self.request, "You do not have an active order")
 			return redirect("shops:order-summary")
 
-@login_required
+# @login_required
 @csrf_exempt
 def callbackurl(request):
 	# def get(self, *args, **kwargs):
 	# 	# def callbackurl(self, request, *args, **kwargs):
 	# from django.contrib.auth.models import AnonymousUser
 	# user = AnonymousUser()
-	user = request.user
+	user = request.user.is_authenticated
 	print(user)
 	# 	print(current_user.username)
 	# return HttpResponse("Welcome to poll's index!")
@@ -395,7 +395,7 @@ def callbackurl(request):
 	status = pay()
 	print(status)
 
-	callback = Mpesapay.objects.get(user = request.user, cash='notpayed')
+	callback = Mpesapay.objects.get(request.user.is_authenticated, cash='notpayed')
 	callback.update(cash=status)
 		
 	if status == 'Paid':
@@ -404,7 +404,7 @@ def callbackurl(request):
 		# def get(self, *args, **kwargs):
 		# order = Order.objects.filter(user = request.user, ordered='False')
 		# print(order)
-		order = Order.objects.get(user = request.user, ordered=False)
+		order = Order.objects.get(request.user.is_authenticated, ordered=False)
 		order.update(ordered=True)
 		for item in order:
 			item.save()
